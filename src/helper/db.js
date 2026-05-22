bf8597756880647e412aba5243bcc875f42d77fd
sachinsoumya <pandasoumyasachin@gmail.com>
2026-05-22 17:59:55 +0530

    created user schema , model and connected to mongodb

--- src/helper/db.js
+++ src/helper/db.js
@@ -1,16 +1,26 @@
 import mongoose from "mongoose";
+import {User} from "@/models/user";
 
 export const connectToDB = async () => {
   try {
-    const {connections} = await mongoose.connect(process.env.MONGO_DB_URI, {
-      dbName: " TaskManager",
+    const { connections } = await mongoose.connect(process.env.MONGO_DB_URI, {
+      dbName: "Task_manager",
     });
 
-    console.log(connections);
-
+    // console.log(connections);
 
     console.log("Connected to DB successfully");
-    console.log("Connected to the host:" , connections[0].host);
+
+    const user1 = new User({
+      name: "Soumya",
+      email: "soumya@gmail.com",
+      password: "123456",
+      about: "Sachin is a full-stack web developer",
+    });
+
+    await user1.save();
+    console.log("user is created");
+    // console.log("Connected to the host:" , connections[0].host);
   } catch (err) {
     console.log("Error while connecting to Db", err);
   }
---
+++ src/models/user.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    email:{
+        type:String,
+        required:[true , "Email is required"],
+        
+    },
+
+    password: {
+        type:String,
+        required:[true , "password is required"]
+    },
+
+    profileImage:String,
+    about:String,
+
+
+});
+
+export const User = mongoose.model.User || mongoose.model("User" , userSchema);
