28d599cd1c8e4d5eb9b65f7066a3d6c0bd9afc4f
sachinsoumya <pandasoumyasachin@gmail.com>
2026-06-10 11:53:24 +0530

    Created Task , Getting Task , Get a singe task , update the task

--- src/app/api/tasks/[taskId]/route.js
+++ src/app/api/tasks/[taskId]/route.js
@@ -1,17 +1,56 @@
-export async function GET(request){
+import { NextResponse } from "next/server";
+import { connectToDB } from "@/helper/db";
+import { Task } from "@/models/task";
 
+connectToDB();
+
+export async function GET(request, { params }) {
+  const { taskId } = await params;
+
+  try {
+    const task = await Task.findById(taskId);
+
+    console.log(task);
+
+    return NextResponse.json(task);
+  } catch (err) {
+    return NextResponse.json({
+      message: "Error getting task by id",
+      status: false,
+      error: err.message,
+    });
+  }
 }
 
+export async function PUT(request, { params }) {
+  const { taskId } = await params;
 
+  const { title, description, addedDate, status } = await request.json();
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(
+      taskId,
+      {
+        $set: {
+          title,
+          description,
+          status,
+          addedDate,
+        },
+      },
+      { after: true },
+    );
 
-
-
-export async function UPDATE(request){
-
+     return NextResponse.json({
+      message: "Task updated  successfully",
+      data: updatedTask,
+    });
+  } catch (err) {
+    return NextResponse.json({
+      message: "Error while updating the task",
+      status: false,
+      error: err.message,
+    });
+  }
 }
 
-
-
-export async function DELETE(request){
-
-}
+export async function DELETE(request) {}
--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -1,25 +1,52 @@
+import { connectToDB } from "@/helper/db";
 import { NextResponse } from "next/server";
+import { Task } from "@/models/task";
+
+connectToDB();
 
 export async function GET(request) {
-
-  
-
-
   //* get all the tasks
+  try {
+    const tasks = await Task.find({});
 
-
-  
-
- return  NextResponse.json(works);
+    return NextResponse.json(tasks);
+  } catch (err) {
+    return NextResponse.json({
+      message: "Error getting tasks",
+      status: false,
+      error: err.message,
+    });
+  }
 }
 
+export const POST = async (request) => {
+  const { title, description, addedDate, status, userId } =
+    await request.json();
 
+  const taskInstance = new Task({
+    title,
+    description,
+    addedDate,
+    status,
+    userId,
+  });
 
-export const POST = async (request)=>{
+  try {
+    const newTask = await taskInstance.save();
 
+    console.log(newTask);
 
-  //* create a task and save it to database
-  return NextResponse.json({
-
-  })
-}
+    //* create a task and save it to database
+    return NextResponse.json({
+      message: "Task craeted successfully",
+      status: true,
+      data: newTask,
+    });
+  } catch (err) {
+    return NextResponse.json({
+      message: "Error creating task",
+      status: false,
+      error: err.message,
+    });
+  }
+};
